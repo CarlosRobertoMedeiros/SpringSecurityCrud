ff18e8d1882fbec80de483dc605f6b066a85e9cf
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2019-07-24 06:22:48 -0300

    A RestController Simple Crud App

---
+++ src/main/java/com/robertodev/springsecuritycrud/SpringSecurityCrudApplication.java
@@ -0,0 +1,15 @@
+package com.robertodev.springsecuritycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCrudApplication.class, args);
+	}
+
+}
+
+
